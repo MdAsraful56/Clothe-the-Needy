d48b6f84ef2c7943f850b8229362ab0b800cd919
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-03 15:02:46 +0600

    carusel work done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-const Main = () => {
+const Carousel = () => {
 
 
 
@@ -73,4 +73,4 @@ const Main = () => {
     );
 };
 
-export default Main;
+export default Carousel;
