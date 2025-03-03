0dd50a14c32d48ae2d4be2248460216d36456e6a
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-04 05:50:28 +0600

    login password show & heeden

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 const Login = () => {
+
+    const [showPassword, setShowPassword] = useState(false);
+    
+    const handleLogin = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
+
+
     return (
         <div>
             <div className="container mx-auto p-16 my-10 space-y-16"> 
@@ -12,15 +25,21 @@ const Login = () => {
                 </div>
                 <div className=" text-black mx-64 py-5 border-2 bg-white border-gray-200 rounded-lg p-5">
                     <h2 className="text-2xl text-blue-500 mb-3 font-bold">Login Form</h2>
-                    <div className="">
+                    <div onSubmit={handleLogin} className="">
                         <form className="space-y-5">
                             <div className="flex flex-col space-y-2">
                                 <label htmlFor="email">Email</label>
-                                <input type="email" id="email" name="email" className="border-2 border-gray-200 p-2 rounded-lg" />
+                                <input type="email" id="email" name="email" placeholder='example@mail.com' className="border-2 border-gray-200 p-2 rounded-lg" />
                             </div>
-                            <div className="flex flex-col space-y-2">
+                            <div className="flex flex-col space-y-2 relative">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" id="password" name="password" className="border-2 border-gray-200 p-2 rounded-lg" />
+                                <input type={showPassword ? 'text' : 'password'} id="password" name="password" className="border-2 border-gray-200 p-2 rounded-lg" placeholder='Password' />
+                                <button className="absolute right-3 top-12" onClick={() => setShowPassword(!showPassword)}>
+                                    {/* <FaEye /> */}
+                                    {
+                                        showPassword ? <FaEyeSlash /> : <FaEye />
+                                    }
+                                </button>
                             </div>
                             <div className="flex flex-col space-y-2">
                                 <button className="bg-blue-500 text-white p-2 rounded-lg">Login</button>
