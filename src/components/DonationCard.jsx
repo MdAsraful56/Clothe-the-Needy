cba94383f4ca0a69a7acbcb48fcad7388207f1f3
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-07 08:40:30 +0600

    donation page complete

--- src/components/DonationCard.jsx
+++ src/components/DonationCard.jsx
@@ -5,16 +5,25 @@ const DonationCard = ({donation}) => {
     const {title, description, img, contacts } = donation;
 
     return (
-        <div className='space-y-5 bg-white text-black rounded-xl p-5 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>
+        <div className='space-y-5 bg-white text-black rounded-xl p-5 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105'>
             <div className="w-full h-72  flex justify-center items-center overflow-hidden">
                 <img src={img} alt="" className="rounded-xl w-full h-full object-cover" />
             </div>
             <div className="">
                 <h2 className="text-3xl">{title}</h2>
-                <p className="">{description}</p>
+                <p className="">{description}</p> 
+                <br />
+                <div className="flex flex-row gap-3 text-sm">
+                    <p className="">
+                        {contacts.email}
+                    </p>
+                    <p className="">
+                        {contacts.phone}
+                    </p>
+                </div>
             </div>
             <div className="flex items-center justify-center">
-                <button className="btn">DONATE NOW</button>
+                <button className="btn bg-[#090979] rounded-lg hover:bg-[#790976] ">DONATE NOW</button>
             </div>
         </div>
     );
--- src/layout/DonationLayout.jsx
+++ src/layout/DonationLayout.jsx
@@ -21,7 +21,7 @@ const DonationLayout = () => {
             <Navbar />
             <Outlet />
             <div className="container mx-auto lg:p-16 lg:my-10 md:p-5 p-2  my-2 space-y-16">
-                <div className="grid gap-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+                <div className="grid gap-8 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 p-2">
                     {
                         donations.map((donation, index) => <DonationCard key={index} donation={donation} /> )
                     }
