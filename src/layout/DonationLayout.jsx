ba37dbb75b867f6cfc99f5a557453684b975c68e
MdAsraful56 <work.mdasraful56@gmail.com>
2025-03-07 07:23:05 +0600

    donation data json

--- src/layout/DonationLayout.jsx
+++ src/layout/DonationLayout.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router';
 import Footer from '../components/Footer';
 
 const DonationLayout = () => {
+
+    const [donations, setDonations] = useState([]);
+
+    useEffect( ()=>{
+        fetch('../../public/Donation.json')
+    } ,[])
+
+
     return (
         <div>
             <Navbar />
             <Outlet />
+
+
+
+
             <Footer />
         </div>
     );
